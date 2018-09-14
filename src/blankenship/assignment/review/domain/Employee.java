20897a37eb7b92ae2017ff12981b88bd680cca6f
Karl Blankenship <Karleb777@gmail.com>
2018-09-14 17:18:37 -0500

    Initial revision of Employee Class.

--- src/blankenship/assignment/review/domain/Employee.java
+++ src/blankenship/assignment/review/domain/Employee.java
@@ -6,9 +6,64 @@
 package blankenship.assignment.review.domain;
 
 /**
- *
+ * Employee class which will be used for holding the employee name and
+ * the employee manager name.
  * @author Karl
  */
 public class Employee {
     
+    /*
+    Create private variables to hold employee name and employee manager.
+    */
+    private String name;
+    private String manager;
+    
+    /**
+     * Default constructor.
+     */
+    public Employee() {
+    }
+
+    /**
+     * Overload constructor.
+     * @param name
+     * @param manager 
+     */
+    public Employee(String name, String manager) {
+        this.name = name;
+        this.manager = manager;
+    }
+
+    /**
+     * Accessor method for getting employee name.
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+    
+    /**
+     * Mutator method for setting employee name.
+     * @param name 
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    /**
+     * Accessor method for getting employee manager name.
+     * @return manager
+     */
+    public String getManager() {
+        return manager;
+    }
+
+    /**
+     * Mutator method for getting employee manager name.
+     * @param manager 
+     */
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+         
 }
