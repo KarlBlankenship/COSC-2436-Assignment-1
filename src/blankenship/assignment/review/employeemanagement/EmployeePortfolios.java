8aafcad84e66558f8b942e48a884761a0ca1dd4b
Karl Blankenship <Karleb777@gmail.com>
2018-09-15 08:44:36 -0500

    initial attempt at reading the file and writing data.

--- src/blankenship/assignment/review/employeemanagement/EmployeePortfolios.java
+++ src/blankenship/assignment/review/employeemanagement/EmployeePortfolios.java
@@ -9,6 +9,7 @@ import blankenship.assignment.review.domain.Employee;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
         
 
@@ -35,7 +36,8 @@ public class EmployeePortfolios {
         //ef.display(ef.sortByManagerName(List));
     }
     
-    private List<Employee> loadEmployeePortfolios(String filename) throws FileNotFoundException {
+    private List<Employee> loadEmployeePortfolios(String filename) 
+            throws FileNotFoundException, IOException {
         
         // Create List to hold Employee objects.
         List<Employee> employeeList = new ArrayList();
@@ -47,6 +49,20 @@ public class EmployeePortfolios {
         FileReader fr = new FileReader(fileName);
         // Read file into a buffer.
         BufferedReader br = new BufferedReader(fr);
+        // Create String variable to hold each buffered line.
+        String line = br.readLine();
+        // Write all buffered lines into Employee Instances and write to List.
+        while (line != null) {
+            // Create an instance of Employee.
+            Employee em = new Employee();
+            // Separate out name and manager from line.
+            String[] lineEM = line.split(","); // Split line into employee and manager.
+            em.setName(lineEM[0]);
+            em.setManager(lineEM[1]);
+            employeeList.add(em);
+            // Read next line of file.
+            line = br.readLine();
+        }
         
         return employeeList;
     }
