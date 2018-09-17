35988b076a5771fe4516a5cfb467111b9c2d8da4
Karl Blankenship <Karleb777@gmail.com>
2018-09-17 11:35:09 -0500

    Cleaning up code in EmployeePortfolios and adding comments.

--- src/blankenship/assignment/review/employeemanagement/EmployeePortfolios.java
+++ src/blankenship/assignment/review/employeemanagement/EmployeePortfolios.java
@@ -1,75 +1,101 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * COSC 2436 Programming Fundamentals III with Java
+ * Java Recap Assignment 1
+ * Create an Employee Class containing employee name and manager name.
+ * Create an EmployeePortfolios class to read a file containing a list of
+ * employee names and their managers name. Then write to methods to sort the
+ * employee objects by name and by manager name and display the results.
  */
+
 package blankenship.assignment.review.employeemanagement;
 
 import blankenship.assignment.review.domain.Employee;
-
 import java.io.*;
 import java.util.*;
         
-
 /**
- *
- * @author Karl
+ * EmployeePortfolios Class containing the main class for executing this 
+ * program. Also contains the NameComparator and Manager Comparator classes
+ * as well and the methods for sorting by name, sorting by manager and
+ * displaying the results.
+ * @author Karl Blankenship
  */
 public class EmployeePortfolios {
 
     /**
-     * @param args the command line arguments
-     * @throws java.io.IOException
+     * Main method for program execution.
+     * @param args
+     * @throws IOException 
      */
     public static void main(String[] args) throws IOException {
-        System.out.println("main is executing");
+        
         // Create an EmployeePortfolios object.
         EmployeePortfolios ef = new EmployeePortfolios();
     
         // Create an employee list variable.
-        List<Employee> employees = new ArrayList();
-    
-        // Start writing required commands....
-        // employeeData.txt is stored in main project file so that it can
-        // be accessed without a specific file path.
+        List<Employee> employees;
+        
+        /*
+            employeeData.txt is stored in main project file so that it can
+            be accessed without a specific file path for simplicity.
+        */
+        // Load employee data.
         employees = ef.loadEmployeePortfolios("employeeData.txt");
+        
+        // Display unsorted employee instances
+        System.out.println("Unsorted list of employees.");
         ef.display(employees);
+        
+        // Display list of employees sorted by name.
+        System.out.println("List of employees sorted by name.");
         ef.display(ef.sortByEmployeeName(employees));
+        
+        // Display list of employees sorted by manager.
+        System.out.println("List of employees sorted by manager.");
         ef.display(ef.sortByManagerName(employees));
     }
     
+    /**
+     * loadEmployeePortfolios Method will read the text file, create 
+     * employee instances, write text data to instance fields and add
+     * instances to the output list.
+     * @param filename
+     * @return employeeList
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
     private List<Employee> loadEmployeePortfolios(String filename) 
             throws FileNotFoundException, IOException {
         
-        // Create List to hold Employee objects.
+        // Create List to hold Employee objects and for return.
         List<Employee> employeeList = new ArrayList();
         
-        // Create String variable to hold input filename.
-        String fileName = filename;
- 
         // Create FileReader object and pass in file.
-        FileReader fr = new FileReader(fileName);
+        FileReader fr = new FileReader(filename);
+        
         // Read file into a buffer.
         BufferedReader br = new BufferedReader(fr);
+        
         // Create String var to hold each buffered line and burn file header.
-        String line = br.readLine();
+        String line = br.readLine(); // Reads in file header.
         line = br.readLine(); // Read first line of data after header.
+        
         // Write all buffered lines into Employee Instances and write to List.
         while (line != null) {
             // Create an instance of Employee.
             Employee em = new Employee();
-            // Separate out name and manager from line.
-            String[] lineEM = line.split(","); // Split into employee and manager.
+            // Split out name and manager from line.
+            String[] lineEM = line.split(","); 
+            // Set instance fields.
             em.setName(lineEM[0]);
             em.setManager(lineEM[1]);
+            // Add the employee instance to the List.
             employeeList.add(em);
             // Read next line of file.
             line = br.readLine();
         }
-       
-        System.out.println(employeeList.get(0).getName()); 
         
-        return employeeList;
+        return employeeList; // Return employee list.
     }
     
     private void display(List<Employee> inputList) {
